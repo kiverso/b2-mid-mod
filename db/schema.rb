66a2f7c675edae0e9a0eb6b6c9721c96f9c3bf3f
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-22 09:32:59 -0600

    Create models for first user_story

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200522152721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "genre"
+    t.string "creation_year"
+  end
+
+  create_table "studios", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
