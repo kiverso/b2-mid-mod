5456ba358c28b6c0f72ba554fd4c644f59b85ff6
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-22 11:34:56 -0600

    Add functionality for story 2

--- spec/features/movies/show_page_spec.rb
+++ spec/features/movies/show_page_spec.rb
@@ -19,11 +19,15 @@ RSpec.describe "Movie show page", type: :feature do
     actor3 = Actor.create(name: "Actor3",
                           age: 30)
 
+    ActorMovie.create(actor_id: actor1.id, movie_id: movie1.id )
+    ActorMovie.create(actor_id: actor2.id, movie_id: movie1.id )
+    ActorMovie.create(actor_id: actor3.id, movie_id: movie1.id )
+
     visit("movies/#{movie1.id}")
     expect(page).to have_content('Movie1')
     expect(page).to have_content('Action')
     expect(page).to have_content('1987')
-    expect(page).to have_content("Actors: #{Actor2.name}, #{Actor2.name}, #{Actor2.name}")
+    expect(page).to have_content("Actors: #{actor2.name}, #{actor1.name}, #{actor3.name}")
     expect(page).to have_content("Average actor age: 24.33")
   end
 end
